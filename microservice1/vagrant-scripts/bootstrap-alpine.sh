e56d3b9b017dd18280aa5215daf51e86c91f9c90
theprotos <vlcz@seznam.cz>
2022-08-30 00:24:10 +0200

    lab-k8s: readme

--- microservice1/vagrant-scripts/bootstrap-alpine.sh
+++ microservice1/vagrant-scripts/bootstrap-alpine.sh
@@ -10,7 +10,7 @@ systemctl reload sshd
 echo -e "\n  [$(date +'%Y-%m-%dT%H:%M:%S%z')]: Install docker"
 #apk add --update docker docker-compose openr
 
-#addgroup username docker
+#addgroup vagrant docker
 #rc-update add docker boot
 #service docker start
 
